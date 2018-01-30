86c6717283ef982349add4f53b560ef8c69a1e9f
Catalina Cano <catalinacano08@hotmail.com>
2018-01-29 21:00:35 -0500

    Funcionando el Backend server

--- models/administrador.js
+++ models/administrador.js
@@ -7,14 +7,14 @@ var Schema = mongoose.Schema;
 // Definir el Administrador esquema
 // Funcion que recibe un objeto de js con la configuracion del registro
 var administradorSchema = new Schema({
-    nombre: { type: String, required: [true, 'Este Campo es Requerido'] }
-    /*email: {
+    nombre: { type: String, required: [true, 'Este Campo es Requerido'] },
+    email: {
         type: String,
         unique: true,
         required: [true, 'Este Campo es Requerido']
     },
     password: { type: String, required: [true, 'Este Campo es Requerido'] },
-    role: { type: String, required: [true, 'Este Campo es Requerido'], default: 'ESTUDIANTE_ROL' }*/
+    role: { type: String, required: [true, 'Este Campo es Requerido'], default: 'ADMIN_ROL' }
 });
 
 
