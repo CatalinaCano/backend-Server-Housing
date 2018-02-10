e73f8f470830f1de1376601ac720df7ab00565c4
Catalina Cano <catalinacano08@hotmail.com>
2018-02-09 23:46:22 -0500

    Servicios asincronos para la busqueda con paginacion y populate

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -6,6 +6,7 @@ var app = express(); // Levantar la app
 
 
 var Estudiante = require('../models/estudiante');
+var Alojamiento = require('../models/alojamiento');
 
 // Rutas
 app.get('/todo/:busqueda', (req, res, next) => {
@@ -13,31 +14,58 @@ app.get('/todo/:busqueda', (req, res, next) => {
     var busqueda = req.params.busqueda;
     //Expresion regular para poder buscar
     var regex = new RegExp(busqueda, 'i');
+    var desde = req.query.desde || 0;
+    desde = Number(desde);
 
-    buscarEnEstudiante(busqueda, regex)
-        .then(sedes => {
+
+    Promise.all([
+            buscarEnEstudiante(busqueda, regex),
+            buscarEnAlojamiento(busqueda, regex)
+        ])
+        .then(respuestas => {
             res.status(202).json({
                 ok: true,
                 mensaje: 'Petición realizada correctamente',
-                sedes: sedes
+                sedes: respuestas[0],
+                sedesCercanas: respuestas[1]
             }); // Todo se hizo corriendo correctamente
-        });
+        })
 });
 
 
 function buscarEnEstudiante(busqueda, regex) {
-
     return new Promise((resolve, reject) => {
         Estudiante.find({ perteneceA: regex }, (err, sedes) => {
             if (err) {
                 reject('Error al cargar las sedes de los estudiantes');
             } else {
-                resolve(sedes)
+                resolve(sedes);
             }
         });
     });
+}
 
+function buscarEnAlojamiento(busqueda, regex, desde) {
 
+    return new Promise((resolve, reject) => {
+        Alojamiento.find({}, 'propiedadesAlojamiento.estadoAlojamiento estudiante')
+            .populate('estudiante', 'role email')
+            .skip(desde)
+            .limit(5)
+            .or([{ 'sedeCercana': regex }, { 'hospedanA': regex }])
+            .exec((err, alojamientos) => {
+                if (err) {
+                    reject('Erro al cargar alojamientos');
+                } else {
+
+                    Alojamiento.count({}, (err, total) => {
+                        resolve(alojamientos, total);
+                    })
+
+                }
+
+            })
+    });
 }
 
 // Exporatacion para hacer uso de ella en cualquier modulo
