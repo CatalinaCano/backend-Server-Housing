2b4d0c325ff091d24c5e1ca8c7107b6bd80fbb68
Catalina Cano <catalinacano08@hotmail.com>
2018-02-14 21:51:03 -0500

    Google

---
+++ config/config.js
@@ -0,0 +1,3 @@
+// Google
+module.exports.GOOGLE_CLIENT_ID = '430294334235 - ab0t1b1e45bml213k82lld46ng1s9dfk.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'uZCNkNOMVgLEQw-qgjKBwsXy';
--- routes/login.js
+++ routes/login.js
@@ -5,6 +5,42 @@ var app = express(); // Levantar la app
 //Importar modelo Estudiante
 var Estudiante = require('../models/estudiante');
 
+var GoogleAuth = require('google-auth-library');
+var { google } = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+
+// Autenticacion de google
+app.post('/google', (req, res, next) => {
+
+    var token = req.body.token || 'XXX';
+
+    var oauth2Client = new OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
+
+    oauth2Client.verifyIdToken({
+            idToken: token,
+            audience: GOOGLE_CLIENT_ID
+        },
+        function(e, login) {
+
+            if (error) return console.error(error);
+            var payload = login.getPayload();
+            var userid = payload['sub'];
+
+
+            res.status(200).json({
+                ok: true,
+                payload: payload
+            });
+
+        }
+    )
+
+});
+
+
 
 app.post('/', (req, res) => {
     var body = req.body;
